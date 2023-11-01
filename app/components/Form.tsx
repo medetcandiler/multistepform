868e7f7f4a1225f93eb9275155e9537028ba0a30
Medetcan Diler <medetdiler1996@gmail.com>
2023-11-01 17:08:31 +0300

    Implemented zod validation to the forms

--- app/components/Form.tsx
+++ app/components/Form.tsx
@@ -2,23 +2,34 @@
 import { useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { z } from "zod";
-import { zodResolver } from '@hookform/resolvers/zod'
+import { zodResolver } from "@hookform/resolvers/zod";
+
 import { FcNext } from "react-icons/fc";
 import { FcPrevious } from "react-icons/fc";
-
+import { LiaAngleDownSolid } from "react-icons/lia";
 
 import { formSchema } from "@/libs/schema";
 
 type Inputs = z.infer<typeof formSchema>;
 
 const steps = [
-  { id: "first step", name: "personal details" },
-  { id: "second step", name: "address information" },
+  {
+    id: "first step",
+    name: "personal details",
+    field: ["firstName", "lastName", "email"],
+  },
+  {
+    id: "second step",
+    name: "address information",
+    field: ["country", "street", "city", "state", "zip"],
+  },
   { id: "third step", name: "conguratulations" },
 ];
 
 const Form = () => {
   const [currentStep, setCurrentStep] = useState(0);
+  const [isNextStep, setIsNextStep] = useState<boolean>(false);
+  console.log(isNextStep)
   const {
     register,
     handleSubmit,
@@ -26,26 +37,36 @@ const Form = () => {
     trigger,
     formState: { errors },
   } = useForm<Inputs>({
-    resolver: zodResolver(formSchema)
+    resolver: zodResolver(formSchema),
   });
 
   const onSubmit: SubmitHandler<Inputs> = (data) => {
     console.log("data", data);
-    console.log("errors", errors);
     reset();
   };
 
+  type fildName = keyof Inputs;
+
   const next = async () => {
-    // if (currentStep < steps.length - 1) {
-    //   setCurrentStep((prev) => prev + 1);
-    // }
-    const output = await trigger("firstName");
-    console.log(output);
+    const fields = steps[currentStep].field;
+    const output = await trigger(fields as fildName[], {
+      shouldFocus: true,
+    });
+    if (!output) return;
+    if (currentStep < steps.length - 1) {
+      if(currentStep == steps.length - 2){
+        handleSubmit(onSubmit)()
+      }
+      setCurrentStep((prev) => prev + 1);
+      setIsNextStep(true)
+    }
   };
 
   const prev = () => {
     if (currentStep > 0) {
       setCurrentStep((prev) => prev - 1);
+      setIsNextStep(false)
+      
     }
   };
 
@@ -87,7 +108,7 @@ const Form = () => {
       </nav>
 
       {currentStep === 0 && (
-        <section className="flex flex-col space-y-12">
+        <section className={`flex flex-col space-y-12`}>
           <div className="flex flex-col space-y-3">
             <h1 className="font-bold text-lg">Personal information</h1>
             <h3>Please provide your personal details.</h3>
@@ -96,18 +117,23 @@ const Form = () => {
             onSubmit={handleSubmit(onSubmit)}
             className="flex flex-col space-y-10"
           >
-            <div className="flex justify-between gap-5">
+            <div className="flex justify-between gap-24">
               <div className="relative z-0 w-full mb-6 group">
                 <input
                   type="text"
                   id="firstName"
-                  className="block py-2.5 px-0 w-full  text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none   focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                  className={`block py-2.5 px-0 w-full  text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none   focus:outline-none focus:ring-0 focus:border-blue-600 peer ${
+                    errors.firstName && "border-red-600 focus:border-red-600"
+                  }`}
                   placeholder=" "
-                  {...register("firstName", { required: true })}
+                  {...register("firstName")}
+                  autoComplete="off"
                 />
                 <label
                   htmlFor="firstName"
-                  className="peer-focus:font-medium absolute  text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                  className={`peer-focus:font-medium absolute  text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 ${
+                    errors.firstName && "peer-focus:text-red-600 text-red-600"
+                  }`}
                 >
                   First name
                 </label>
@@ -116,13 +142,18 @@ const Form = () => {
                 <input
                   type="text"
                   id="lastName"
-                  className="block py-2.5 px-0 w-full  text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none   focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                  className={`block py-2.5 px-0 w-full  text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none   focus:outline-none focus:ring-0 focus:border-blue-600 peer ${
+                    errors.lastName && "border-red-600 focus:border-red-600"
+                  }`}
                   placeholder=" "
                   {...register("lastName")}
+                  autoComplete="off"
                 />
                 <label
-                  htmlFor="lastname"
-                  className="peer-focus:font-medium absolute  text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                  htmlFor="lastName"
+                  className={`peer-focus:font-medium absolute  text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 ${
+                    errors.lastName && "peer-focus:text-red-600 text-red-600"
+                  }`}
                 >
                   Last name
                 </label>
@@ -133,53 +164,66 @@ const Form = () => {
               <input
                 type="email"
                 id="email"
-                className="block py-2.5 px-0 w-full  text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none   focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                className={`block py-2.5 px-0 w-full  text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none   focus:outline-none focus:ring-0 focus:border-blue-600 peer ${
+                  errors.email && "border-red-600 focus:border-red-600"
+                }`}
                 placeholder=" "
                 {...register("email")}
+                autoComplete="off"
               />
               <label
                 htmlFor="email"
-                className="peer-focus:font-medium absolute  text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                className={`peer-focus:font-medium absolute  text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 ${
+                  errors.email && "peer-focus:text-red-600 text-red-600"
+                }`}
               >
                 Email address
               </label>
             </div>
-            <button onClick={() => trigger()}>trigger</button>
           </form>
         </section>
       )}
       {currentStep === 1 && (
-        <section className="flex flex-col space-y-12">
+        <section className={`flex flex-col space-y-12`}>
           <div className="flex flex-col space-y-3">
             <h1 className="font-bold text-lg">Address information</h1>
             <h3>Please provide your address information.</h3>
           </div>
           <form className="flex flex-col">
-            <div className="relative z-0 w-1/2 mb-6 group">
-              {/*  */}
+            <div className="relative w-1/2 mb-6 group">
               <select
                 id="country"
-                className="block w-full py-2.5 pr-3 text-gray-900 bg-transparent border-b-2 border-gray-300  focus:outline-none   appearance-none"
+                className="block w-full py-2.5 pr-3  bg-transparent border-b-2 border-gray-300  focus:outline-none   appearance-none cursor-pointer"
+                defaultValue={"none"}
+                {...register("country")}
               >
-                <option value="" disabled selected hidden>
+                <option className="text-gray-600" value="none">
                   Select Country
                 </option>
                 <option value="tr">Turkey</option>
                 <option value="us">United States</option>
                 <option value="ca">Canada</option>
               </select>
-              {/*  */}
+              <div className="absolute right-3 top-1/2 -translate-y-1/2 pointer-events-none">
+                <LiaAngleDownSolid />
+              </div>
             </div>
             <div className="relative z-0 w-full mb-6 group">
               <input
-                type="email"
+                type="text"
                 id="street"
-                className="block py-2.5 px-0 w-full  text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none   focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                className={`block py-2.5 px-0 w-full  text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none   focus:outline-none focus:ring-0 focus:border-blue-600 peer ${
+                  errors.street && "border-red-600 focus:border-red-600"
+                }`}
                 placeholder=" "
+                {...register("street")}
+                autoComplete="off"
               />
               <label
                 htmlFor="street"
-                className="peer-focus:font-medium absolute  text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 capitalize"
+                className={`peer-focus:font-medium absolute  text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 ${
+                  errors.street && "peer-focus:text-red-600 text-red-600"
+                }`}
               >
                 street address
               </label>
@@ -190,12 +234,18 @@ const Form = () => {
                 <input
                   type="text"
                   id="city"
-                  className="block py-2.5 px-0 w-full  text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none   focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                  className={`block py-2.5 px-0 w-full  text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none   focus:outline-none focus:ring-0 focus:border-blue-600 peer ${
+                    errors.city && "border-red-600 focus:border-red-600"
+                  }`}
                   placeholder=" "
+                  {...register("city")}
+                  autoComplete="off"
                 />
                 <label
                   htmlFor="city"
-                  className="peer-focus:font-medium absolute  text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                  className={`peer-focus:font-medium absolute  text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 ${
+                    errors.city && "peer-focus:text-red-600 text-red-600"
+                  }`}
                 >
                   City
                 </label>
@@ -204,12 +254,18 @@ const Form = () => {
                 <input
                   type="text"
                   id="state"
-                  className="block py-2.5 px-0 w-full  text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none   focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                  className={`block py-2.5 px-0 w-full  text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none   focus:outline-none focus:ring-0 focus:border-blue-600 peer ${
+                    errors.state && "border-red-600 focus:border-red-600"
+                  }`}
                   placeholder=" "
+                  {...register("state")}
+                  autoComplete="off"
                 />
                 <label
                   htmlFor="state"
-                  className="peer-focus:font-medium absolute  text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                  className={`peer-focus:font-medium absolute  text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 ${
+                    errors.state && "peer-focus:text-red-600 text-red-600"
+                  }`}
                 >
                   State / Province
                 </label>
@@ -218,12 +274,18 @@ const Form = () => {
                 <input
                   type="text"
                   id="zip"
-                  className="block py-2.5 px-0 w-full  text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none   focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                  className={`block py-2.5 px-0 w-full  text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none   focus:outline-none focus:ring-0 focus:border-blue-600 peer ${
+                    errors.zip && "border-red-600 focus:border-red-600"
+                  }`}
                   placeholder=" "
+                  {...register("zip")}
+                  autoComplete="off"
                 />
                 <label
                   htmlFor="zip"
-                  className="peer-focus:font-medium absolute  text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                  className={`peer-focus:font-medium absolute  text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 ${
+                    errors.zip && "peer-focus:text-red-600 text-red-600"
+                  }`}
                 >
                   ZIP
                 </label>
--- libs/schema.tsx
+++ libs/schema.tsx
@@ -3,5 +3,10 @@ import { z } from "zod";
 export const formSchema = z.object({
   firstName: z.string().min(1, 'this field is required'),
   lastName: z.string().min(1, 'this field is required'),
-  email: z.string().min(1, 'this field is required').email()
+  email: z.string().min(1, 'this field is required').email(),
+  country: z.string().min(1, 'this field is required'),
+  street: z.string().min(1, 'this field is required'),
+  city: z.string().min(1, 'this field is required'),
+  state: z.string().min(1, 'this field is required'),
+  zip: z.string().min(1, 'this field is required')
 })
